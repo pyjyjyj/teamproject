c46b721258f387ab052595c0a15f87d34adcdee0
pyjyjyj <duswn9694@gmail.com>
2017-10-14 15:10:13 +0900

    메뉴리스트뷰와 전화버튼

--- app/src/main/java/com/example/jessie/teamproject/MainActivity.java
+++ app/src/main/java/com/example/jessie/teamproject/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.jessie.teamproject;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    static MenuAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ArrayList<MenuItem> data = new ArrayList<MenuItem>();
+        data.add(new MenuItem(R.drawable.momil, "냉모밀", "3000"));
+        data.add(new MenuItem(R.drawable.coolnoodle, "냉면", "2500"));
+        data.add(new MenuItem(R.drawable.porkdubu, "돈육순두부찌개", "3800"));
+        data.add(new MenuItem(R.drawable.gimbab, "야채김밥", "1200"));
+
+        adapter = new MenuAdapter(this, R.layout.menu_item, data);
+
+        ListView listView = (ListView)findViewById(R.id.menuList);
+        final View menuHeader = getLayoutInflater().inflate(R.layout.menu_header, null, false) ;
+        listView.addHeaderView(menuHeader);
+        listView.setAdapter(adapter);
+
+        listView.setDividerHeight(8);
+    }
+
+    public void callButtonClick(View view) {
+        String num = getString(R.string.phone_number);
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+num));
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/jessie/teamproject/MenuAdapter.java
@@ -0,0 +1,73 @@
+package com.example.jessie.teamproject;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Jessie on 2017-10-14.
+ */
+
+class MenuItem {
+    int menuIcon;
+    String menuName;
+    String menuPrice;
+    MenuItem(int menuIcon, String menuName, String menuPrice) {
+        this.menuIcon = menuIcon;
+        this.menuName = menuName;
+        this.menuPrice = menuPrice;
+    }
+}
+
+public class MenuAdapter extends BaseAdapter {
+    private Context mContext;
+    private int mResource;
+    private ArrayList<MenuItem> mItems = new ArrayList<>();
+
+    public MenuAdapter(Context context, int resource, ArrayList<MenuItem> items) {
+        mContext = context;
+        mResource = resource;
+        mItems = items;
+    }
+
+    @Override
+    public int getCount() {
+        return mItems.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(mResource, parent, false);
+        }
+
+        ImageView icon = (ImageView) convertView.findViewById(R.id.menuIcon);
+        icon.setImageResource(mItems.get(position).menuIcon);
+
+        TextView menuName = (TextView) convertView.findViewById(R.id.menuName);
+        menuName.setText(mItems.get(position).menuName);
+
+        TextView menuPrice = (TextView) convertView.findViewById(R.id.menuPrice);
+        menuPrice.setText(mItems.get(position).menuPrice);
+
+        return convertView;
+    }
+}
