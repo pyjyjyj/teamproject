54807a7acd523edafc7f952cdc6fc23375ce9f3d
kohyejihs <ko.hjha@gmail.com>
2017-11-14 15:46:04 +0900

    RestaurantInsertActivity 생성과 MainActivity의 버튼 클릭 시 RestaurantInsertActivity로 이동

--- app/src/main/java/com/example/jessie/teamproject/MainActivity.java
+++ app/src/main/java/com/example/jessie/teamproject/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.jessie.teamproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button restaurant_insert = (Button) findViewById(R.id.restaurant_insert);
+        restaurant_insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RestaurantInsertActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
