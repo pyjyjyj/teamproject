78617ed920c402f429209c3aa0176aac18bc44bd
kohyejihs <ko.hjha@gmail.com>
2017-11-16 17:18:10 +0900

    createRestaurantDataArray 수정

--- app/src/main/java/com/example/jessie/teamproject/RestaurantInsertActivity.java
+++ app/src/main/java/com/example/jessie/teamproject/RestaurantInsertActivity.java
@@ -6,28 +6,32 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class RestaurantInsertActivity extends AppCompatActivity {
-    ImageButton imageButton = (ImageButton) findViewById(R.id.imageButton);
-    EditText rName = (EditText) findViewById(R.id.rName);
-    EditText rAddress = (EditText) findViewById(R.id.rAddress);
-    EditText rPhone = (EditText) findViewById(R.id.rPhone);
-    EditText rStartTime = (EditText) findViewById(R.id.rStartTime);
-    EditText rEndTime = (EditText) findViewById(R.id.rEndTime);
+    ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_insert);
+
+        imageButton = (ImageButton) findViewById(R.id.imageButton);
         String[] Restaurant_Data = createRestaurantDataArray();
     }
 
+    private String mPhotoFileName = null;
+
     private String[] createRestaurantDataArray() {
+        EditText rName = (EditText) findViewById(R.id.rName);
+        EditText rAddress = (EditText) findViewById(R.id.rAddress);
+        EditText rPhone = (EditText) findViewById(R.id.rPhone);
+        EditText rStartTime = (EditText) findViewById(R.id.rStartTime);
+        EditText rEndTime = (EditText) findViewById(R.id.rEndTime);
         String name = rName.getText().toString();
         String address = rAddress.getText().toString();
         String phone = rPhone.getText().toString();
         //String.format() : https://kwanulee.github.io/Android/data-management/sqlite.html 참조
         String time = String.format("%s ~ %s", rStartTime.getText().toString(), rEndTime.getText().toString());
-        String img = ""; //추가예정
-        String[] Restaurant_Data = {name, address, phone, time, img};
+        String imgFileName = mPhotoFileName;
+        String[] Restaurant_Data = {name, address, phone, time, imgFileName};
         return Restaurant_Data;
     }
 }
