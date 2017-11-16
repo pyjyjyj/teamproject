39cf798f68b0f8a3ee0d3ef6a5721b76416ca374
kohyejihs <ko.hjha@gmail.com>
2017-11-16 17:25:10 +0900

    퍼미션을 체크하는 checkDangerousPermission() 생성

--- app/src/main/java/com/example/jessie/teamproject/RestaurantInsertActivity.java
+++ app/src/main/java/com/example/jessie/teamproject/RestaurantInsertActivity.java
@@ -1,5 +1,9 @@
 package com.example.jessie.teamproject;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -12,6 +16,7 @@ public class RestaurantInsertActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_insert);
+        checkDangerousPermissions();
 
         imageButton = (ImageButton) findViewById(R.id.imageButton);
         String[] Restaurant_Data = createRestaurantDataArray();
@@ -33,5 +38,20 @@ public class RestaurantInsertActivity extends AppCompatActivity {
         String imgFileName = mPhotoFileName;
         String[] Restaurant_Data = {name, address, phone, time, imgFileName};
         return Restaurant_Data;
+    }
+
+    final int REQUEST_EXTERNAL_STORAGE_FOR_MULTIMEDIA=1;
+    private void checkDangerousPermissions() {
+        String[] permissions = { Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE };
+        int permissionCheck = PackageManager.PERMISSION_GRANTED;
+        for (int i = 0; i < permissions.length; i++) {
+            permissionCheck = ContextCompat.checkSelfPermission(this, permissions[i]);
+            if (permissionCheck == PackageManager.PERMISSION_DENIED)
+                break;
+        }
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_EXTERNAL_STORAGE_FOR_MULTIMEDIA);
+        }
     }
 }
