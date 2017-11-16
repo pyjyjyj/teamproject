22d126487bbe346d1e35112ae3438b58175755e6
kohyejihs <ko.hjha@gmail.com>
2017-11-16 17:38:24 +0900

    카메라 버튼 클릭 시 사진 촬영 및 저장, 촬영한 사진을 이미지버튼에 표시하는 기능 추가

--- app/src/main/java/com/example/jessie/teamproject/RestaurantInsertActivity.java
+++ app/src/main/java/com/example/jessie/teamproject/RestaurantInsertActivity.java
@@ -1,13 +1,26 @@
 package com.example.jessie.teamproject;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+/* currentDateFormat(), dispatchTakePictureIntent(), onActivityResult(), checkDangerousPermissions()
+* 출처:https://kwanulee.github.io/Android/multimedia/multimedia.html*/
 
 public class RestaurantInsertActivity extends AppCompatActivity {
     ImageButton imageButton;
@@ -19,10 +32,52 @@ public class RestaurantInsertActivity extends AppCompatActivity {
         checkDangerousPermissions();
 
         imageButton = (ImageButton) findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+
         String[] Restaurant_Data = createRestaurantDataArray();
     }
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private File mPhotoFile =null;
     private String mPhotoFileName = null;
+
+    private String currentDateFormat(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HH_mm_ss");
+        String currentTimeStamp = dateFormat.format(new Date());
+        return currentTimeStamp;
+    }
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            //1. 카메라 앱으로 찍은 이미지를 저장할 파일 객체 생성
+            mPhotoFileName = "IMG"+currentDateFormat()+".jpg";
+            mPhotoFile = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), mPhotoFileName);
+            if (mPhotoFile != null) {
+                //2. 생성된 파일 객체에 대한 Uri 객체를 얻기
+                Uri imageUri = FileProvider.getUriForFile(this, "com.example.jessie.teamproject", mPhotoFile);
+                //3. Uri 객체를 Extras를 통해 카메라 앱으로 전달
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            } else
+                Toast.makeText(getApplicationContext(), "file null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            if (mPhotoFileName != null) {
+                mPhotoFile = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), mPhotoFileName);
+                Uri uri = Uri.fromFile(mPhotoFile);
+                imageButton.setImageURI(uri);
+            } else
+                Toast.makeText(getApplicationContext(), "mPhotoFile is null", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     private String[] createRestaurantDataArray() {
         EditText rName = (EditText) findViewById(R.id.rName);
