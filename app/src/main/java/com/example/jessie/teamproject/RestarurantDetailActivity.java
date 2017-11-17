8a8078cf84800143c356842405bcc872c5269846
pyjyjyj <vpfpzk@naver.com>
2017-11-17 12:45:03 +0900

    액션바 생성후 클릭시 MenuinsertActivity로 이동

--- app/src/main/java/com/example/jessie/teamproject/RestarurantDetailActivity.java
+++ app/src/main/java/com/example/jessie/teamproject/RestarurantDetailActivity.java
@@ -6,6 +6,8 @@ import android.net.Uri;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
@@ -66,6 +68,19 @@ public class RestarurantDetailActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.restaurant_detail_actionbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(android.view.MenuItem item) {
+        startActivity(new Intent(this, MenuInsertActivity.class));
+        return true;
     }
 
     public void callButtonClick(View view) {
