15b527f7da9044b1c0fd255d9327c9ef712ff699
pyjyjyj <vpfpzk@naver.com>
2017-11-17 13:08:36 +0900

    MenuInsertActivity생성 및 화면디자인

---
+++ app/src/main/java/com/example/jessie/teamproject/MenuInsertActivity.java
@@ -0,0 +1,13 @@
+package com.example.jessie.teamproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuInsertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_insert);
+    }
+}
