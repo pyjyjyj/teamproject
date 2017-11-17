8ef3a5f2b8ac542a0407be629bba7741219993bf
pyjyjyj <vpfpzk@naver.com>
2017-11-17 11:19:31 +0900

    createRestaurantDataArray()를 이용해 받은 배열을 데이터베이스에 저장 후 액티비티 이동

--- app/src/main/java/com/example/jessie/teamproject/RestaurantInsertActivity.java
+++ app/src/main/java/com/example/jessie/teamproject/RestaurantInsertActivity.java
@@ -3,6 +3,7 @@ package com.example.jessie.teamproject;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -11,7 +12,9 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -24,12 +27,15 @@ import java.util.Date;
 
 public class RestaurantInsertActivity extends AppCompatActivity {
     ImageButton imageButton;
+    private DBHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_insert);
         checkDangerousPermissions();
+
+        mDbHelper = new DBHelper(this);
 
         imageButton = (ImageButton) findViewById(R.id.imageButton);
         imageButton.setOnClickListener(new View.OnClickListener(){
@@ -39,7 +45,13 @@ public class RestaurantInsertActivity extends AppCompatActivity {
             }
         });
 
-        String[] Restaurant_Data = createRestaurantDataArray();
+        Button restaurant_insert_btn = (Button) findViewById(R.id.restaurant_insert_btn);
+        restaurant_insert_btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                insertRestaurant();
+            }
+        });
+
     }
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
@@ -91,14 +103,13 @@ public class RestaurantInsertActivity extends AppCompatActivity {
         //String.format() : https://kwanulee.github.io/Android/data-management/sqlite.html 참조
         String time = String.format("%s ~ %s", rStartTime.getText().toString(), rEndTime.getText().toString());
         String imgFileName = mPhotoFileName;
-        String[] Restaurant_Data = {name, address, phone, time, imgFileName};
-        return Restaurant_Data;
+        String[] dataArray = {name, address, phone, time, imgFileName};
+        return dataArray;
     }
 
     final int REQUEST_EXTERNAL_STORAGE_FOR_MULTIMEDIA=1;
     private void checkDangerousPermissions() {
-        String[] permissions = { Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE };
+        String[] permissions = { Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE };
         int permissionCheck = PackageManager.PERMISSION_GRANTED;
         for (int i = 0; i < permissions.length; i++) {
             permissionCheck = ContextCompat.checkSelfPermission(this, permissions[i]);
@@ -108,5 +119,17 @@ public class RestaurantInsertActivity extends AppCompatActivity {
         if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, permissions, REQUEST_EXTERNAL_STORAGE_FOR_MULTIMEDIA);
         }
+    }
+
+    String[] Restaurant_Data;
+    private void insertRestaurant() {
+        Restaurant_Data = createRestaurantDataArray();
+        long nOfRows = mDbHelper.insertRestaurantByMethod(Restaurant_Data[0], Restaurant_Data[1], Restaurant_Data[2], Restaurant_Data[3], Restaurant_Data[4]);
+        Intent intent = new Intent(this, RestarurantDetailActivity.class);
+        startActivity(intent);
+        if (nOfRows > 0)
+            Toast.makeText(this, nOfRows + " Record Inserted", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this, "No Record Inserted", Toast.LENGTH_SHORT).show();
     }
 }
