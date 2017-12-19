bad75945aa69a1dc98f7e7d693dcaa2e8a61156f
pyjyjyj <vpfpzk@naver.com>
2017-12-19 12:28:50 +0900

    Main & Menu Activity 코딩 정리

--- app/src/main/java/com/example/jessie/teamproject/MainActivity.java
+++ app/src/main/java/com/example/jessie/teamproject/MainActivity.java
@@ -1,18 +1,12 @@
 package com.example.jessie.teamproject;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,39 +20,6 @@ public class MainActivity extends AppCompatActivity {
 
         Start = (ImageView) findViewById(R.id.ani);
 
-       /* mDbHelper = new DBHelper(this);
-
-        Button restaurant_insert = (Button) findViewById(R.id.restaurant_insert);
-        restaurant_insert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), RestaurantInsertActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Cursor cursor = mDbHelper.getAllRestaurantsByMethod(); // 전체 레스토랑 정보를 db 에서 가져옴
-        String[] restaurants = new String[cursor.getCount()]; // 레스토랑 이름을 저장하기 위한 String 배열
-        int i = 0;
-
-        while (cursor.moveToNext()) { // 모든 레스토랑 이름을 배열에 저장하는 반복문
-            restaurants[i++] = cursor.getString(1).toString();
-        }
-
-        *//* 리스트뷰 강의자료에서 참조 *//*
-        final ArrayAdapter<String> adapt = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, restaurants);
-
-        ListView restaurant_list = (ListView)findViewById(R.id.restaurant_list);
-        restaurant_list.setAdapter(adapt);
-
-        restaurant_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(getApplicationContext(), RestarurantDetailActivity.class);
-                intent.putExtra("resName", adapt.getItem(i).toString());
-                startActivity(intent);
-            }
-        });*/
         startTwinAnimation();
         Handler mHandler = new Handler();
         mHandler.postDelayed(new Runnable(){
--- app/src/main/java/com/example/jessie/teamproject/MenuFragment.java
+++ app/src/main/java/com/example/jessie/teamproject/MenuFragment.java
@@ -8,11 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.File;
-import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
